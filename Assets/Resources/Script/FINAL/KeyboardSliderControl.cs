c296ee0c23a3001de38c019757572012bc5c7dc9
Raj Gaurav <rajgaurav2k21@gmail.com>
2024-06-25 01:02:09 +0530

    last_previous

---
+++ Assets/Resources/Script/FINAL/KeyboardSliderControl.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KeyboardSliderControl : MonoBehaviour
+{
+    public Slider slider;
+    public float increment = 1f;
+    public GetFeedback_Block getFeedback_Block;
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            IncreaseSlider();
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            DecreaseSlider();
+        }
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+           getFeedback_Block.Feedback();
+        }
+    }
+
+    void IncreaseSlider()
+    {
+        if (slider != null)
+        {
+            slider.value += increment * Time.deltaTime;
+            if (slider.value > slider.maxValue)
+            {
+                slider.value = slider.maxValue;
+            }
+        }
+    }
+
+    void DecreaseSlider()
+    {
+        if (slider != null)
+        {
+            slider.value -= increment * Time.deltaTime;
+            if (slider.value < slider.minValue)
+            {
+                slider.value = slider.minValue;
+            }
+        }
+    }
+}
