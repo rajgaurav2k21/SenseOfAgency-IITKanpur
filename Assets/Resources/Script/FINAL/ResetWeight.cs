7755aad20bcca0b6dd6432ebe4f4de6961b85ee9
Raj Gaurav <rajgaurav2k21@gmail.com>
2024-07-09 05:21:59 +0530

    pilot chnages

--- Assets/Resources/Script/FINAL/ResetWeight.cs
+++ Assets/Resources/Script/FINAL/ResetWeight.cs
@@ -6,24 +6,22 @@ public class ResetWeight : MonoBehaviour
 {
     public GameObject weight1;
     public GameObject weight2;
+    public GameObject weight1ResetPoint;
+    public GameObject weight2ResetPoint;
     public bool reset = false;
 
     void Update()
     {
         if (reset)
         {
-            // Reset positions
-            weight1.transform.position = new Vector3(0.317000002f,0.0472999997f,0.00300000003f);
-            weight2.transform.position = new Vector3(-0.0419999994f,0.0472999997f,-0.0109999999f);
+            weight1.transform.position = weight1ResetPoint.transform.position;
+            weight2.transform.position = weight2ResetPoint.transform.position;
 
-            // Reset rotations
-            weight1.transform.rotation = Quaternion.Euler(0,267.709106f,0);
-            weight2.transform.rotation = Quaternion.Euler(0,267.709106f,0);
+            weight1.transform.rotation = weight1ResetPoint.transform.rotation;
+            weight2.transform.rotation = weight2ResetPoint.transform.rotation;
 
-            // Set reset to false to prevent continuous resetting
             reset = false;
 
-            // Optional: Log to confirm reset has been completed
             Debug.Log("Weights have been reset.");
         }
     }
